8fb9c9cc61a62705eacee75cdcd550cb16358642
Elizabeth Gardiner <liz.gardiner@btinternet.com>
2023-11-06 10:44:22 +0000

    add more subreddits

--- src/data/subredditInfo.js
+++ src/data/subredditInfo.js
@@ -6,24 +6,42 @@ import proghum from "../assets/programmerhumor.png";
 import salsa from "../assets/salsa.png";
 import gaming from "../assets/gaming.png";
 import piano from "../assets/piano.png";
+import askUk from "../assets/askUk.png";
+import cats from "../assets/cats.png";
+import mildlyInteresting from "../assets/mildlyInteresting.png";
+import madeMeSmile from "../assets/madeMeSmile.png";
+import unresolvedMysteries from "../assets/unresolvedMysteries.png";
+import books from "../assets/books.png";
+import iAmA from "../assets/iAmA.png";
 
 export const subreddits = [
-  {
-    name: "AskReddit",
-    img: askReddit,
-    endpoint: "AskReddit/",
-  },
   {
     name: "AmItheAsshole",
     img: AITA,
     endpoint: "AmItheAsshole/",
   },
   {
-    name: "todayilearned",
-    img: til,
-    endpoint: "todayilearned/",
+    name: "AskReddit",
+    img: askReddit,
+    endpoint: "AskReddit/",
   },
+  { name: "AskUk", img: askUk, endpoint: "AskUk/" },
+  { name: "books", img: books, endpoint: "books/" },
+  { name: "cats", img: cats, endpoint: "cats/" },
+  {
+    name: "gaming",
+    img: gaming,
+    endpoint: "gaming/",
+  },
+  { name: "IAmA", img: iAmA, endpoint: "iama/" },
+  { name: "MadeMeSmile", img: madeMeSmile, endpoint: "mademesmile/" },
   { name: "memes", img: memes, endpoint: "memes" },
+  {
+    name: "mildlyinteresting",
+    img: mildlyInteresting,
+    endpoint: "mildlyinteresting/",
+  },
+  { name: "piano", img: piano, endpoint: "piano/" },
   {
     name: "ProgrammerHumor",
     img: proghum,
@@ -31,9 +49,14 @@ export const subreddits = [
   },
   { name: "Salsa", img: salsa, endpoint: "Salsa/" },
   {
-    name: "gaming",
-    img: gaming,
-    endpoint: "gaming/",
+    name: "todayilearned",
+    img: til,
+    endpoint: "todayilearned/",
   },
-  { name: "piano", img: piano, endpoint: "piano/" },
+
+  {
+    name: "UnresolvedMysteries",
+    img: unresolvedMysteries,
+    endpoint: "unresolvedmysteries/",
+  },
 ];
